2b9f219ff644ee59ce26b6d44d792873d00bce36
wuxiaohui <https://git.oschina.net/aguardman/zhaoshurenAPP.git>
2017-03-31 11:31:52 +0800

    添加视频加载框架   暂时是本地获取视频源  需要修改

--- app/src/main/java/io/dcloud/h52927aa1/Acticity/demo.java
+++ app/src/main/java/io/dcloud/h52927aa1/Acticity/demo.java
@@ -1,8 +1,10 @@
 package io.dcloud.h52927aa1.Acticity;
 
+import android.app.Activity;
+
 /**
  * Created by wuxiaohui on 17/3/31.
  */
 
-public class demo {
+public class demo extends Activity{
 }
